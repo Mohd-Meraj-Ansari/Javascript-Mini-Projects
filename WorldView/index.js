4341febe7f7a145b9b8ffd6b26c15d7eaee5e32c
Meraj Ansari <meraj6316@gmail.com>
2025-04-02 11:49:01 +0530

    added the feature of navigating back to previous page and also added the filter by region feature

--- WorldView/index.js
+++ WorldView/index.js
@@ -1,9 +1,9 @@
 const countryContainer = document.querySelector(".container");
+const filterRegion = document.querySelector("select");
 
 fetch("https://restcountries.com/v3.1/all")
   .then((response) => response.json())
   .then((data) => {
-    
     data.forEach((country) => {
       // console.log(country.capital);
       const countryCard = document.createElement("a");
@@ -17,7 +17,7 @@ fetch("https://restcountries.com/v3.1/all")
               "en-IN"
             )}</p>
             <p><b>Region: </b> ${country.region}</p>
-            <p><b>Capital: </b> ${country.capital.join(', ')}</p>
+            <p><b>Capital: </b> ${country.capital}</p>
           </div>`;
 
       countryCard.innerHTML = cardHtml;
@@ -25,3 +25,34 @@ fetch("https://restcountries.com/v3.1/all")
       countryContainer.append(countryCard);
     });
   });
+
+filterRegion.addEventListener("change", (e) => {
+  console.log(e.target.value);
+
+  fetch(`https://restcountries.com/v3.1/region/${e.target.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      countryContainer.innerHTML = "";
+      data.forEach((country) => {
+        // console.log(country.capital);
+        const countryCard = document.createElement("a");
+        countryCard.classList.add("country-card");
+        countryCard.href = `/WorldView/country.html?name=${country.name.common}`;
+
+        const cardHtml = `<img src="${country.flags.svg}" alt="flag" />
+            <div class="card-content">
+              <h4 class="c-name">${country.name.common}</h4>
+              <p><b>Population: </b> ${country.population.toLocaleString(
+                "en-IN"
+              )}</p>
+              <p><b>Region: </b> ${country.region}</p>
+              <p><b>Capital: </b> ${country.capital}</p>
+            </div>`;
+
+        countryCard.innerHTML = cardHtml;
+
+        countryContainer.append(countryCard);
+      });
+    });
+});
