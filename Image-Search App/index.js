d4a51a33f6028740d36da2565eb21e8b47162dc3
Meraj Ansari <meraj6316@gmail.com>
2025-05-16 22:30:33 +0530

    created files and added styles to it

---
+++ Image-Search App/index.js
@@ -0,0 +1,4 @@
+const formData = document.getElementById('search-image');
+const data = document.getElementById('input-field');
+const contentArea = document.getElementsByClassName('content-area');
+const showMoreBtn = document.getElementById('show-more');
