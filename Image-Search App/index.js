439e2bb84e6e25ef9a7118a628f218e368b1aa98
Meraj Ansari <meraj6316@gmail.com>
2025-05-17 14:07:26 +0530

    Added the functionality of image search and added responsiveness to the application

--- Image-Search App/index.js
+++ Image-Search App/index.js
@@ -1,4 +1,51 @@
-const formData = document.getElementById('search-image');
-const data = document.getElementById('input-field');
-const contentArea = document.getElementsByClassName('content-area');
-const showMoreBtn = document.getElementById('show-more');
+const formData = document.getElementById("search-image");
+const data = document.getElementById("input-field");
+const contentArea = document.getElementById("content-area");
+const showMoreBtn = document.getElementById("show-more");
+const searchArea = document.getElementById('search-area')
+
+
+let page = 1;
+const accessKey = "";
+
+async function getData() {
+  const query = data.value;
+  const URL = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${accessKey}&per_page=12`;
+
+  let response, responseData;
+
+    response = await fetch(URL)
+    responseData = await response.json();
+  
+  if (page === 1) {
+    contentArea.innerHTML = "";
+  }
+
+  const results = responseData.results;
+
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+
+    contentArea.appendChild(imageLink);
+  });
+
+  showMoreBtn.style.display = "block";
+}
+
+formData.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  getData();
+});
+
+showMoreBtn.addEventListener("click", () => {
+  page++;
+  getData();
+});
